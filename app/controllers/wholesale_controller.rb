8d6d77c34852d13e4cdcc975df262fa6bb214c25
Jeremy Purser <jeremypurser@gmail.com>
2019-02-26 10:30:31 -0500

    Updates style

--- app/controllers/wholesale_controller.rb
+++ app/controllers/wholesale_controller.rb
@@ -1,10 +1,14 @@
 class WholesaleController < ApplicationController
   def all_products
+    @products = Product.all
   end
 
   def products_by_category
+
   end
 
   def products_by_vendor
+    @vendor = User.find(params[:user_id])
+    @products = Product.where(user_id: params[:user_id])
   end
 end
