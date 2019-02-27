f76a727100eecff9dedcce30212717b4dc0e5dd3
Jeremy Purser <jeremypurser@gmail.com>
2019-02-27 09:06:42 -0500

    Set back buttons to back

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,6 +11,7 @@ class ProductsController < ApplicationController
   # GET /products/1
   # GET /products/1.json
   def show
+    @product = Product.find(params[:id])
   end
 
   # GET /products/new
