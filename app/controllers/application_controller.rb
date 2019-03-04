5118a998829a7a94428922716a3148df222c0d24
Jeremy Purser <jeremypurser@gmail.com>
2019-03-03 20:35:21 -0500

    Redirect to login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
     # Prevent CSRF attacks by raising an exception.
     # For APIs, you may want to use :null_session instead.
+    before_action :authenticate_user!
     before_action :vendors
     before_action :categories
     before_action :emojis
