27171f2a1c2f152b93849c5d2712cd31266611fc
Jeremy Purser <jeremypurser@gmail.com>
2019-03-03 20:06:20 -0500

    Add roles to Users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,7 +32,7 @@ class ApplicationController < ActionController::Base
     protected
 
     def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name, :bio, :location, :avatar, :company])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:username, :name, :bio, :location,:avatar, :company])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name, :bio, :location, :avatar, :company, :role])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:username, :name, :bio, :location,:avatar, :company, :role])
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_26_162252) do
+ActiveRecord::Schema.define(version: 2019_03_03_234901) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2019_02_26_162252) do
     t.string "location"
     t.string "avatar"
     t.text "following"
+    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
