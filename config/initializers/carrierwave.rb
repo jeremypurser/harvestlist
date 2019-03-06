c55fe0868ffc57ae713d929bf6a226cf452125a4
Jeremy Purser <jeremypurser@gmail.com>
2019-03-06 16:47:47 -0500

    Change config to Google

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,11 @@
 CarrierWave.configure do |config|
- config.fog_credentials = {
-   :provider => "AWS",
-   :aws_access_key_id => "#{ENV["AWS_ACCESS_KEY_ID"]}",
-   :aws_secret_access_key => "#{ENV["AWS_SECRET_ACCESS_KEY"]}",
-   :region => "#{ENV["S3_REGION"]}",
-   :path_style => true,
-   :host => "s3-#{ENV["S3_REGION"]}.amazonaws.com",
-   :endpoint => "https://s3-#{ENV["S3_REGION"]}.amazonaws.com",
- }
+config.fog_credentials = {
 
- config.fog_directory = "#{ENV["S3_BUCKET_NAME"]}"
- config.cache_dir = "#{Rails.root}/tmp/uploads"
+    :provider                         => 'Google',
+    :google_storage_access_key_id     => "#{ENV["google_storage_access_key_id"]}",
+    :google_storage_secret_access_key => "#{ENV["google_storage_secret_access_key"]}"  
+
+    }
+
+    config.fog_directory = 'jpurser-harvestlist'
 end
