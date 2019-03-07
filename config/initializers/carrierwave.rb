8f54d1c4586e77fa98b613e809763ebc2fc63706
Jeremy Purser <jeremypurser@gmail.com>
2019-03-06 21:36:20 -0500

    Update carrierwave config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,4 +7,7 @@ CarrierWave.configure do |config|
       region:                "#{ENV['S3_REGION']}",
   }
   config.fog_directory  = "#{ENV['S3_BUCKET']}"
+  config.cache_dir = "#{Rails.root}/tmp/uploads"
+  config.asset_host = "https://s3-#{ENV["S3_REGION"]}.amazonaws.com"
+
 end  
